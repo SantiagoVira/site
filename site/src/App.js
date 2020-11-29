98f39b46cfd0527d8b89020e8dc262c2450f913f
SantiagoVira <8bitharrypotter@gmail.com>
2020-11-28 21:53:32 -0500

    navbar

--- site/src/App.js
+++ site/src/App.js
@@ -1,25 +1,74 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import { withStyles } from "@material-ui/core/styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PersonIcon from "@material-ui/icons/Person";
+import FolderIcon from "@material-ui/icons/Folder";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import HomeIcon from "@material-ui/icons/Home";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "home",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event, newValue) {
+    this.setState({ value: newValue });
+  }
+
+  render() {
+    const actionClasses = this.props.classes;
+
+    return (
+      <BottomNavigation
+        value={this.state.value}
+        onChange={this.handleChange}
+        classes={actionClasses}
+      >
+        <BottomNavigationAction
+          label="Home"
+          value="home"
+          icon={<HomeIcon />}
+          classes={actionClasses}
+        />
+        <BottomNavigationAction
+          label="Favorites"
+          value="favorites"
+          icon={<FavoriteIcon />}
+          classes={actionClasses}
+        />
+        <BottomNavigationAction
+          label="All Projects"
+          value="all"
+          icon={<FolderIcon />}
+          classes={actionClasses}
+        />
+        <BottomNavigationAction
+          label="Me"
+          value="me"
+          icon={<PersonIcon />}
+          classes={actionClasses}
+        />
+      </BottomNavigation>
+    );
+  }
 }
 
-export default App;
+const styles = {
+  root: {
+    background: "#282c34",
+    color: "#fff",
+    "&$selected": {
+      color: "red",
+    },
+  },
+  selected: {},
+};
+
+export default withStyles(styles)(App);
