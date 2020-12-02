d3d762f327b05a4ae23dcccd0c9e3b55957bd33b
SantiagoVira <8bitharrypotter@gmail.com>
2020-12-02 12:45:23 -0500

    fix navbar wordbreak

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,69 +10,69 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import HomeIcon from "@material-ui/icons/Home";
 
 class Nav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: this.props.value.toString(),
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: this.props.value.toString(),
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  handleChange(event, newValue) {
-    this.setState({ value: newValue });
-    document.location.href = "/" + newValue;
-  }
+    handleChange(event, newValue) {
+        this.setState({ value: newValue });
+        document.location.href = "/" + newValue;
+    }
 
-  render() {
-    const actionClasses = this.props.classes;
+    render() {
+        const actionClasses = this.props.classes;
 
-    return (
-      <BottomNavigation
-        value={this.state.value}
-        onChange={this.handleChange}
-        classes={actionClasses}
-        id="NavBar"
-      >
-        <BottomNavigationAction
-          label="Home"
-          value="Home"
-          icon={<HomeIcon />}
-          classes={actionClasses}
-        />
-        <BottomNavigationAction
-          label="My Favorites"
-          value="Favorite"
-          icon={<FavoriteIcon />}
-          classes={actionClasses}
-        />
-        <BottomNavigationAction
-          label="All Projects"
-          value="All"
-          icon={<FolderIcon />}
-          classes={actionClasses}
-        />
-        <BottomNavigationAction
-          label="Me"
-          value="Me"
-          icon={<PersonIcon />}
-          classes={actionClasses}
-        />
-      </BottomNavigation>
-    );
-  }
+        return (
+            <BottomNavigation
+                value={this.state.value}
+                onChange={this.handleChange}
+                classes={actionClasses}
+                id="NavBar"
+            >
+                <BottomNavigationAction
+                    label="Home"
+                    value="Home"
+                    icon={<HomeIcon />}
+                    classes={actionClasses}
+                />
+                <BottomNavigationAction
+                    label="Favorites"
+                    value="Favorite"
+                    icon={<FavoriteIcon />}
+                    classes={actionClasses}
+                />
+                <BottomNavigationAction
+                    label="All Projects"
+                    value="All"
+                    icon={<FolderIcon />}
+                    classes={actionClasses}
+                />
+                <BottomNavigationAction
+                    label="Me"
+                    value="Me"
+                    icon={<PersonIcon />}
+                    classes={actionClasses}
+                />
+            </BottomNavigation>
+        );
+    }
 }
 
 const styles = {
-  root: {
-    background: getComputedStyle(document.body)
-      .getPropertyValue("--bg")
-      .toString(),
-    color: "#fff",
-    "&$selected": {
-      color: "red",
+    root: {
+        background: getComputedStyle(document.body)
+            .getPropertyValue("--bg")
+            .toString(),
+        color: "#fff",
+        "&$selected": {
+            color: "red",
+        },
     },
-  },
-  selected: { color: "red" },
+    selected: { color: "red" },
 };
 
 export default withStyles(styles)(Nav);
