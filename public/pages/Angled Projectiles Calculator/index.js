5173384e021cc721c1dc9c20924c0c88b425ec8c
SantiagoVira <8bitharrypotter@gmail.com>
2020-12-08 11:53:36 -0500

    Added process, favicon, and title

--- public/pages/Angled Projectiles Calculator/index.js
+++ public/pages/Angled Projectiles Calculator/index.js
@@ -8,28 +8,69 @@ function handle() {
         timeify();
     }
 }
+let Vi = "V<sub class='s'>i</sub>";
+let sqr = "<sup class='s'>2</sup>";
 
 function rangeify() {
     let vel = document.getElementById("vel").value;
     let ang = document.getElementById("ang").value;
-    let result =
-        2 * (vel * Math.cos(r(ang))) * ((vel * Math.sin(r(ang))) / 9.81);
+    let result = (
+        2 *
+        (vel * Math.cos(r(ang))) *
+        ((vel * Math.sin(r(ang))) / 9.81)
+    ).toFixed(2);
     document.getElementById("Answer").innerHTML =
-        +result.toFixed(2).toString() + " meters";
+        +result.toString() + " meters";
+
+    document.getElementById(
+        "Process"
+    ).innerHTML = `2 x (${Vi} x cos(angle)) x ((${Vi} x sin(angle)) / 9.81) =<br>
+            2 x (${vel} x cos(${ang})) x ((${vel} x sin(${ang})) / 9.81) =<br>
+            2 x (${vel} x ${+Math.cos(r(ang)).toFixed(
+        2
+    )}) x ((${vel} x ${+Math.sin(r(ang)).toFixed(2)}) / 9.81) =<br>
+            2 x ${+(vel * Math.cos(r(ang))).toFixed(2)} x (${+(
+        vel * Math.sin(r(ang))
+    ).toFixed(2)} / 9.81) =<br>
+            2 x ${+(vel * Math.cos(r(ang))).toFixed(2)} x ${+(
+        (vel * Math.sin(r(ang))) /
+        9.81
+    ).toFixed(2)} =<br>
+            ${result}
+            `;
 }
 function maxheight() {
     let vel = document.getElementById("vel").value;
     let ang = document.getElementById("ang").value;
-    let result = Math.pow(vel * Math.sin(r(ang)), 2) / 19.62;
+    let result = (Math.pow(vel * Math.sin(r(ang)), 2) / 19.62).toFixed(2);
     document.getElementById("Answer").innerHTML =
-        +result.toFixed(2).toString() + " meters";
+        +result.toString() + " meters";
+
+    document.getElementById(
+        "Process"
+    ).innerHTML = `(${Vi} x sin(angle))${sqr} / 19.62 =<br>
+            (${vel} x sin(${ang}))${sqr} / 19.62 =<br>
+            (${vel} x ${+Math.sin(r(ang)).toFixed(2)})${sqr} / 19.62 =<br>
+            (${+(vel * Math.sin(r(ang))).toFixed(2)})${sqr} / 19.62 =<br>
+            ${+Math.pow(vel * Math.sin(r(ang)), 2).toFixed(2)} / 19.62 =<br>
+            ${result}
+            `;
 }
 function timeify() {
     let vel = document.getElementById("vel").value;
     let ang = document.getElementById("ang").value;
-    let result = 2 * ((vel * Math.sin(r(ang))) / 9.81);
+    let result = +(2 * ((vel * Math.sin(r(ang))) / 9.81)).toFixed(2);
     document.getElementById("Answer").innerHTML =
-        +result.toFixed(2).toString() + " seconds";
+        result.toString() + " seconds";
+    document.getElementById(
+        "Process"
+    ).innerHTML = `2 x ((${Vi} x sin(angle)) / 9.81) =<br>
+        2 x ((${vel} x sin(${ang})) / 9.81) =<br>
+        2 x ((${vel} x ${+Math.sin(r(ang)).toFixed(2)}) / 9.81) =<br>
+        2 x (${+(vel * Math.sin(r(ang))).toFixed(2)} / 9.81) =<br>
+        2 x ${+((vel * Math.sin(r(ang))) / 9.81).toFixed(2)} =<br>
+        ${result}
+        `;
 }
 function r(radians) {
     var pi = Math.PI;
