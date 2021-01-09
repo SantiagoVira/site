0c43c95ed079c496cbbeb1e3ef519e69b061c328
SantiagoVira <8bitharrypotter@gmail.com>
2021-01-09 10:56:50 -0500

    Update AllPage.js

--- src/components/AllPage.js
+++ src/components/AllPage.js
@@ -5,23 +5,23 @@ import Typography from "@material-ui/core/Typography";
 import ProjectsDiv from "./ProjectsDiv";
 
 class AllPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="cent">
-        <Nav value="All" /> <p style={{ marginTop: "50px" }}></p>
-        <Typography variant="h3">
-          This is every single JavaScript project I have created
-        </Typography>
-        <Typography variant="h6">(That are worth showing)</Typography>
-        <p style={{ marginTop: "150px" }}></p>
-        <ProjectsDiv all={true} />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="cent">
+                <Nav value="All" /> <p style={{ marginTop: "50px" }}></p>
+                <Typography variant="h3">
+                    These are all of JavaScript projects I have made
+                </Typography>
+                <Typography variant="h6">(That are worth showing)</Typography>
+                <p style={{ marginTop: "150px" }}></p>
+                <ProjectsDiv all={true} />
+            </div>
+        );
+    }
 }
 
 export default AllPage;
