5b73c2f1bb6d01c2c52150e083c00f384611f820
SantiagoVira <8bitharrypotter@gmail.com>
2021-03-16 12:44:39 -0400

    under construction

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Home.css";
+import UnderConstruction from "../../under construction.jpg";
 
 function Home() {
     return (
-        <div>
+        <div className="homepagemain">
             <h1>hey There!</h1>
+            <img src={UnderConstruction} alt="under Construction" />
         </div>
     );
 }
